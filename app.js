e5172b63ed8c29f4abd54e5c67ad3c04d675c984
Hitesh Kumar <hiteshkumarkunri@gmail.com>
2023-07-13 15:43:36 -0700

    updated

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+require("dotenv").config();
 const products_routes = require("./routes/products");
 const connectDB = require("./db/connect");
 
@@ -13,7 +14,7 @@ app.use("/api/products", products_routes);
 
 const start = async () => {
     try{
-        await connectDB();
+        await connectDB(process.env.MONGODB_URL);
         app.listen(PORT, () => {
             console.log(`${PORT} Yes I am connected`);
         });
--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 
-uri = "mongodb+srv://node-api-database:G30ef1gLorXT4Tnk@cluster0.jsoyuxd.mongodb.net/Cluster0?retryWrites=true&w=majority";
-
-const connectDB = () => {
+const connectDB = (uri) => {
     console.log("Database Connected Successfully");
     return mongoose.connect(uri, {
         useNewUrlParser: true,
