9445a155e7a70f39f4a13b01b7df35b6fa7b93ac
Hitesh Kumar <hiteshkumarkunri@gmail.com>
2023-07-13 15:31:59 -0700

    updated

--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
-uri = "";
+uri = "mongodb+srv://node-api-database:G30ef1gLorXT4Tnk@cluster0.jsoyuxd.mongodb.net/Cluster0?retryWrites=true&w=majority";
 
 const connectDB = () => {
+    console.log("Database Connected Successfully");
     return mongoose.connect(uri, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
